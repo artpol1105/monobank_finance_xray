fc13571cc47c2f964ee1ed8ea37acc953d7cf7e9
Artem <artpol1105@gmail.com>
2026-06-03 16:33:59 +0300

    + new MCC codes after historical backfill

--- src/config.py
+++ src/config.py
@@ -20,5 +20,19 @@ mcc_dict = {
     8099: 'Medical Services', 5912: 'Pharmacies', 5122: 'Drugs & Pharmacies',
     5611: 'Men\'s / Boys\' Clothing', 5699: 'Sportswear / Apparel', 7230: 'Barber & Beauty Shops',
 
+    5211: 'Building Materials', 5942: 'Bookstores', 5995: 'Pet Stores', 742: 'Veterinary Services',
+    5311: 'Department Stores', 5200: 'Home Goods', 5399: 'Miscellaneous Goods',
+    4812: 'Telecom Equipment', 5300: 'Wholesale', 5310: 'Discount Stores',
+    5722: 'Household Appliances', 5732: 'Electronics Stores', 5734: 'Software & Digital Services',
+    5931: 'Thrift / Used Goods', 5331: 'Variety Stores', 5964: 'Online Retail / Catalogs',
+    5944: 'Jewelry & Watches', 5039: 'Home Furnishings', 5094: 'Precious Stones & Metals',
+    5992: 'Florists / Flowers', 5993: 'Tobacco / Cigar Stores', 5947: 'Gift / Novelty Shops',
+    5712: 'Furniture Stores', 5946: 'Camera & Photo Supplies', 5262: 'Marketplaces',
 
+    4829: 'Money Transfers', 4814: 'Telecommunication Services', 7399: 'Business & Online Services',
+    8999: 'Postal & Delivery Services', 6012: 'P2P / Financial Transfers', 6536: 'P2P Transfers (Card to Card)',
+    6538: 'Money Transfers', 4816: 'Computer / Network Services', 4215: 'Courier / Postal Services',
+    6011: 'ATM Withdrawals', 6300: 'Insurance Services', 7395: 'Photo Studios',
+    4225: 'Storage / Lockers', 8661: 'Religious Organizations / Charities', 9311: 'Government Services / Taxes',
+    9399: 'Misc Government Services', 9402: 'Postal Services (Government)', 5965: 'Payment Services / Terminals'
 }
